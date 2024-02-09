8818305a014d7e54c981b3c0b455fc5475bade96
amonhaupt <amon.haupt@aol.com>
2024-02-09 13:22:20 +0100

    Test Objekterkennung

--- Facharbeit/script.js
+++ Facharbeit/script.js
@@ -1,3 +1,113 @@
+// OBJEKTERKENNUNG
+
+const demosSection = document.getElementById("demos");
+const startpage_section = document.getElementById("startpage");
+
+var object_model = undefined;
+
+cocoSsd.load().then(function (loadedModel) {
+  object_model = loadedModel;
+  demosSection.classList.remove("invisible");
+  startpage_section.classList.remove("invisible");
+  document.getElementById("still_loading").classList.add("removed");
+});
+
+const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+var children = [];
+
+if (hasGetUserMedia()) {
+  const enableWebcamButton = document.getElementById("webcamButton");
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
+}
+
+function enableCam(event) {
+  if (!object_model) {
+    //console.log("Model not loaded")
+    return;
+  }
+  
+  event.target.classList.add("removed"); 
+  
+  const constraints = {
+    video: {
+        facingMode: { exact: "environment" }
+      }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadeddata", predictWebcam);
+  });
+}
+
+var english_words = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'];
+var german_words = ['Person', 'Fahrrad', 'Auto', 'Motorrad', 'Flugzeug', 'Bus', 'Zug', 'LKW', 'Boot', 'Ampel', 'Feuerhydrant', 'Stoppschild', 'Parkuhr', 'Bank', 'Vogel', 'Katze', 'Hund', 'Pferd', 'Schaf', 'Kuh', 'Elefant', 'tragen', 'Zebra', 'Giraffe', 'Rucksack', 'Regenschirm', 'Handtasche', 'binden', 'Koffer', 'Frisbeescheibe', 'Skier', 'Snowboard', 'Sportball', 'Drachen', 'Baseballschläger', 'Baseballhandschuh', 'Skateboard', 'Surfbrett', 'Tennisschläger', 'Flasche', 'Weinglas', 'Tasse', 'Gabel', 'Messer', 'Löffel', 'Schüssel', 'Banane', 'Apfel', 'Sandwich', 'orange', 'Brokkoli', 'Karotte', 'Hotdog', 'Pizza', 'Krapfen', 'Kuchen', 'Stuhl', 'Couch', 'Topfpflanze', 'Bett', 'Esstisch', 'Toilette', 'Fernseher', 'Laptop', 'Maus', 'Fernbedienung', 'Tastatur', 'Handy', 'Mikrowelle', 'Ofen', 'Toaster', 'Waschbecken', 'Kühlschrank', 'Buch', 'Uhr', 'Vase', 'Schere', 'Teddybär', 'Fön', 'Zahnbürste'];
+
+
+function translate(input) {
+    if (english_words.includes(input)) {
+      var translated_word = german_words[english_words.indexOf(input)];
+      //console.log(translated_word);
+      return translated_word;
+    } else {
+        //console.log(input);
+        return input;
+    }
+}
+
+
+
+function predictWebcam() {
+
+    object_model.detect(video).then(function (predictions) {
+
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+
+    for (let n = 0; n < predictions.length; n++) {
+
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement("p");
+        //console.log(typeof(predictions[n].class));
+        var translation = translate(predictions[n].class);
+        p.innerText = translation  + " - mit " 
+            + Math.round(parseFloat(predictions[n].score) * 100) 
+            + "% Sicherheit";
+
+        p.style = "left: " + predictions[n].bbox[0] + "px;" +
+            "top: " + predictions[n].bbox[1] + "px;" + 
+            "width: " + (predictions[n].bbox[2] - 10) + "px;";
+
+        const highlighter = document.createElement("div");
+        highlighter.setAttribute("class", "highlighter");
+        highlighter.style = "left: " + predictions[n].bbox[0] + "px; top: "
+            + predictions[n].bbox[1] + "px; width: " 
+            + predictions[n].bbox[2] + "px; height: "
+            + predictions[n].bbox[3] + "px;";
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
 // ZIFFERNERKENNUNG
 
 var previous = "home"
@@ -129,8 +239,8 @@ function delete_canvas() {
     }
 
 async function loadModel() {
-    model = undefined; 
-    model = await tf.loadGraphModel("models/model.json");
+    digit_model = undefined; 
+    digit_model = await tf.loadGraphModel("models/model.json");
 }
 loadModel();
 
@@ -149,7 +259,7 @@ async function predict() {
         input_array.push(data[i] / 255);
     }
     console.log(input_array);
-    model.predict([tf.tensor(input_array).reshape([1,28,28,1])]).array().then(function(scores) {
+    digit_model.predict([tf.tensor(input_array).reshape([1,28,28,1])]).array().then(function(scores) {
         scores = scores[0];
         predicted = scores.indexOf(Math.max(...scores));
         console.log(predicted);
@@ -159,19 +269,20 @@ async function predict() {
 
 
 // TIC-TAC-TOE
+// Inspiration: https://www.codebrainer.com/blog/tic-tac-toe-javascript-game (genutzt als Basis)
 
 const cells = document.querySelectorAll(".cell");
 const status_text = document.getElementById("status_text");
 const restart_button = document.getElementById("restart_button");
 const win_conditons = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2], // obere Reihe
+    [3, 4, 5], // mittlere Reihe
+    [6, 7, 8], // untere Reihe
+    [0, 3, 6], // linke Spalte
+    [1, 4, 7], // mittlere Spalte
+    [2, 5, 8], // rechte Spalte
+    [0, 4, 8], // Diagonale links
+    [2, 4, 6]  // Diagonale rechts
 ];
 
 let legal_moves = ["", "", "", "", "", "", "", "", ""];
@@ -230,17 +341,17 @@ function make_ai_move() {
 function check_for_winner() {
     let round_won = false;
     for(let i = 0; i < win_conditons.length; i++) {
-        const condition = win_conditons[i];
-        const cellA = legal_moves[condition[0]];
-        const cellB = legal_moves[condition[1]];
-        const cellC = legal_moves[condition[2]];
+        const condition = win_conditons[i]; // i = 0 -> [0, 1, 2] obere Reihe
+        const cell_1 = legal_moves[condition[0]]; // cell_1 == 0
+        const cell_2 = legal_moves[condition[1]]; // cell_2 == 1
+        const cell_3 = legal_moves[condition[2]]; // cell_3 == 2
 
-        if(cellA == "" || cellB == "" || cellC == "") {
-            continue;
+        if(cell_1 == "" || cell_2 == "" || cell_3 == "") { // (|| <- steht für oder)
+            continue; // wenn eine Zelle oder mehrere leer sind neustarten mit i + 1 
         }
-        if(cellA == cellB && cellB == cellC) {
+        if(cell_1 == cell_2 & cell_2 == cell_3) {
             round_won = true;
-            break;
+            break; // wenn alle Zellen 1 bis 3 gleich sind also z.B. X dann ist die Runde gewonnen
         }
     }
     if(round_won) {

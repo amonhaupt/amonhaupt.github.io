5fe38402bcc4c877dccb0f389b7627c3fe4cc6c5
amonhaupt <amon.haupt@aol.com>
2024-02-12 17:00:59 +0100

    updated model loading

--- Facharbeit/script.js
+++ Facharbeit/script.js
@@ -1,52 +1,78 @@
+var previous = "home"
+var previous_link = "link1"
+
+function show(shown, link) {
+    if (shown == previous) {
+        return false;
+    } else {
+        document.getElementById(shown).style.display = "block";
+        document.getElementById(link).classList.add("active");
+        document.getElementById(previous).style.display = "none";
+        document.getElementById(previous_link).classList.remove("active");
+        previous = shown
+        previous_link = link
+        return false;
+    }
+}
+
 // OBJEKTERKENNUNG
 
-const demosSection = document.getElementById("demos");
+const cam_section = document.getElementById("cam_section");
 const startpage_section = document.getElementById("startpage");
 
 var object_model = undefined;
 
-cocoSsd.load().then(function (loadedModel) {
-  object_model = loadedModel;
-  demosSection.classList.remove("invisible");
-  startpage_section.classList.remove("invisible");
-  document.getElementById("still_loading").classList.add("removed");
-});
+function load_cocoSsd_model() {
+    document.getElementById("still_loading").classList.remove("removed");
+    console.log("test")
+    cocoSsd.load().then(function (loadedModel) {
+        object_model = loadedModel;
+        cam_section.classList.remove("invisible");
+        startpage_section.classList.remove("invisible");
+        document.getElementById("still_loading").classList.add("removed");
+        document.getElementById("load_model_button").classList.add("removed");
+    });
+
+}
+
 
 const video = document.getElementById("webcam");
 const liveView = document.getElementById("liveView");
 
 function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
+    return !!(navigator.mediaDevices &&
+        navigator.mediaDevices.getUserMedia);
 }
 
 var children = [];
 
 if (hasGetUserMedia()) {
-  const enableWebcamButton = document.getElementById("webcamButton");
-  enableWebcamButton.addEventListener("click", enableCam);
+    const enable_webcam_button = document.getElementById("webcam_button");
+    enable_webcam_button.addEventListener("click", enable_cam);
 } else {
-  console.warn("getUserMedia() is not supported by your browser");
+    console.warn("getUserMedia() is not supported by your browser");
 }
 
-function enableCam(event) {
-  if (!object_model) {
-    //console.log("Model not loaded")
-    return;
-  }
-  
-  event.target.classList.add("removed"); 
-  
-  const constraints = {
-    video: {
-        facingMode: { exact: "environment" }
-      }
-  };
+function enable_cam(event) {
+    if (!object_model) {
+        //console.log("Model not loaded")
+        return;
+    }
 
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadeddata", predictWebcam);
-  });
+    event.target.classList.add("removed");
+
+    const constraints = {
+        video: {
+            facingMode: {
+                exact: "environment"
+            }
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predict_webcam);
+    });
 }
 
 var english_words = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'];
@@ -55,9 +81,9 @@ var german_words = ['Person', 'Fahrrad', 'Auto', 'Motorrad', 'Flugzeug', 'Bus', 
 
 function translate(input) {
     if (english_words.includes(input)) {
-      var translated_word = german_words[english_words.indexOf(input)];
-      //console.log(translated_word);
-      return translated_word;
+        var translated_word = german_words[english_words.indexOf(input)];
+        //console.log(translated_word);
+        return translated_word;
     } else {
         //console.log(input);
         return input;
@@ -66,65 +92,49 @@ function translate(input) {
 
 
 
-function predictWebcam() {
+function predict_webcam() {
 
     object_model.detect(video).then(function (predictions) {
 
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
+        for (let i = 0; i < children.length; i++) {
+            liveView.removeChild(children[i]);
+        }
+        children.splice(0);
 
-    for (let n = 0; n < predictions.length; n++) {
+        for (let n = 0; n < predictions.length; n++) {
 
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement("p");
-        //console.log(typeof(predictions[n].class));
-        var translation = translate(predictions[n].class);
-        p.innerText = translation  + " - mit " 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + "% Sicherheit";
+            if (predictions[n].score > 0.66) {
+                const p = document.createElement("p");
+                //console.log(typeof(predictions[n].class));
+                var translation = translate(predictions[n].class);
+                p.innerText = translation + " - mit " +
+                    Math.round(parseFloat(predictions[n].score) * 100) +
+                    "% Sicherheit";
 
-        p.style = "left: " + predictions[n].bbox[0] + "px;" +
-            "top: " + predictions[n].bbox[1] + "px;" + 
-            "width: " + (predictions[n].bbox[2] - 10) + "px;";
+                p.style = "left: " + predictions[n].bbox[0] + "px;" +
+                    "top: " + predictions[n].bbox[1] + "px;" +
+                    "width: " + (predictions[n].bbox[2] - 10) + "px;";
 
-        const highlighter = document.createElement("div");
-        highlighter.setAttribute("class", "highlighter");
-        highlighter.style = "left: " + predictions[n].bbox[0] + "px; top: "
-            + predictions[n].bbox[1] + "px; width: " 
-            + predictions[n].bbox[2] + "px; height: "
-            + predictions[n].bbox[3] + "px;";
+                const highlighter = document.createElement("div");
+                highlighter.setAttribute("class", "highlighter");
+                highlighter.style = "left: " + predictions[n].bbox[0] + "px; top: " +
+                    predictions[n].bbox[1] + "px; width: " +
+                    predictions[n].bbox[2] + "px; height: " +
+                    predictions[n].bbox[3] + "px;";
 
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
+                liveView.appendChild(highlighter);
+                liveView.appendChild(p);
 
-        children.push(highlighter);
-        children.push(p);
-      }
-    }
-    
-    window.requestAnimationFrame(predictWebcam);
-  });
+                children.push(highlighter);
+                children.push(p);
+            }
+        }
+
+        window.requestAnimationFrame(predict_webcam);
+    });
 }
 
 // ZIFFERNERKENNUNG
-
-var previous = "home"
-var previous_link = "link1"
-function show(shown, link) {
-    if (shown == previous) {
-        return false;
-    } else {
-        document.getElementById(shown).style.display="block";
-        document.getElementById(link).classList.add("active");
-        document.getElementById(previous).style.display="none";
-        document.getElementById(previous_link).classList.remove("active");
-        previous = shown
-        previous_link = link
-        return false;
-    }
-}
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 //https://developer.mozilla.org/en-US/docs/Web/API/Path2D
@@ -133,20 +143,26 @@ function show(shown, link) {
 
 var canvas = document.getElementById("drawing_canvas");
 var context = canvas.getContext("2d");
-document.addEventListener("DOMContentLoaded", function() {
-    var mouse_pos = { x: 0, y: 0 };
-    var touch_pos = { x: 0, y: 0};
+document.addEventListener("DOMContentLoaded", function () {
+    var mouse_pos = {
+        x: 0,
+        y: 0
+    };
+    var touch_pos = {
+        x: 0,
+        y: 0
+    };
     let is_drawing = false;
-    
+
     var line_width = 20;
 
     function resize_canvas() {
         if (window.innerHeight > window.innerWidth) {
-            canvas.height = window.innerWidth/2;
-            canvas.width = window.innerWidth/2; //elif window.innerwidth > x == width/3
+            canvas.height = window.innerWidth / 2;
+            canvas.width = window.innerWidth / 2; //elif window.innerwidth > x == width/3
         } else {
-            canvas.height = window.innerHeight/3;
-            canvas.width = window.innerHeight/3;
+            canvas.height = window.innerHeight / 3;
+            canvas.width = window.innerHeight / 3;
         }
         draw(canvas);
     }
@@ -183,7 +199,7 @@ document.addEventListener("DOMContentLoaded", function() {
             context.moveTo(mouse_pos.x, mouse_pos.y);
             get_position(e);
             context.lineTo(mouse_pos.x, mouse_pos.y);
-            context.stroke();                    
+            context.stroke();
         }
     }
 
@@ -192,7 +208,7 @@ document.addEventListener("DOMContentLoaded", function() {
         context.closePath();
         predict();
     }
-    
+
     function get_touch_position(e) {
         touch_pos.x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
         touch_pos.y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
@@ -221,7 +237,7 @@ document.addEventListener("DOMContentLoaded", function() {
             context.moveTo(touch_pos.x, touch_pos.y);
             get_touch_position(e);
             context.lineTo(touch_pos.x, touch_pos.y);
-            context.stroke();                    
+            context.stroke();
         }
     }
 
@@ -234,12 +250,13 @@ document.addEventListener("DOMContentLoaded", function() {
         predict();
     }
 });
+
 function delete_canvas() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 async function loadModel() {
-    digit_model = undefined; 
+    digit_model = undefined;
     digit_model = await tf.loadGraphModel("models/model.json");
 }
 loadModel();
@@ -255,15 +272,15 @@ async function predict() {
     const data = imageData.data;
     var input_array = [];
 
-    for(var i =0; i < data.length; i += 4) {
+    for (var i = 0; i < data.length; i += 4) {
         input_array.push(data[i] / 255);
     }
     console.log(input_array);
-    digit_model.predict([tf.tensor(input_array).reshape([1,28,28,1])]).array().then(function(scores) {
+    digit_model.predict([tf.tensor(input_array).reshape([1, 28, 28, 1])]).array().then(function (scores) {
         scores = scores[0];
         predicted = scores.indexOf(Math.max(...scores));
         console.log(predicted);
-        document.getElementById("prediction_label").textContent=predicted;
+        document.getElementById("prediction_label").textContent = predicted;
     });
 }
 
@@ -282,7 +299,7 @@ const win_conditons = [
     [1, 4, 7], // mittlere Spalte
     [2, 5, 8], // rechte Spalte
     [0, 4, 8], // Diagonale links
-    [2, 4, 6]  // Diagonale rechts
+    [2, 4, 6] // Diagonale rechts
 ];
 
 let legal_moves = ["", "", "", "", "", "", "", "", ""];
@@ -301,10 +318,10 @@ function start_game() {
 function cell_clicked() {
     const cellIndex = this.getAttribute("cell_index");
 
-    if(legal_moves[cellIndex] != "" || !running) {
+    if (legal_moves[cellIndex] != "" || !running) {
         return;
     }
-    if(current_player == "O") {
+    if (current_player == "O") {
         return;
     }
     update_cell(this, cellIndex);
@@ -319,7 +336,7 @@ function update_cell(cell, index) {
 
 function change_player() {
     current_player = (current_player == "X") ? "O" : "X";
-    if(current_player == "O") {
+    if (current_player == "O") {
         setTimeout(make_ai_move, 500);
     }
     status_text.textContent = `${current_player} ist dran`;
@@ -328,10 +345,9 @@ function change_player() {
 function make_ai_move() {
     let move = Math.floor(Math.random() * 9);
     let ai_cell = document.getElementById("cell" + move);
-    if(legal_moves[move] != "") {
+    if (legal_moves[move] != "") {
         make_ai_move();
-    }
-    else {
+    } else {
         legal_moves[move] = current_player;
         ai_cell.textContent = current_player;
         check_for_winner();
@@ -340,29 +356,27 @@ function make_ai_move() {
 
 function check_for_winner() {
     let round_won = false;
-    for(let i = 0; i < win_conditons.length; i++) {
+    for (let i = 0; i < win_conditons.length; i++) {
         const condition = win_conditons[i]; // i = 0 -> [0, 1, 2] obere Reihe
         const cell_1 = legal_moves[condition[0]]; // cell_1 == 0
         const cell_2 = legal_moves[condition[1]]; // cell_2 == 1
         const cell_3 = legal_moves[condition[2]]; // cell_3 == 2
 
-        if(cell_1 == "" || cell_2 == "" || cell_3 == "") { // (|| <- steht für oder)
+        if (cell_1 == "" || cell_2 == "" || cell_3 == "") { // (|| <- steht für oder)
             continue; // wenn eine Zelle oder mehrere leer sind neustarten mit i + 1 
         }
-        if(cell_1 == cell_2 & cell_2 == cell_3) {
+        if (cell_1 == cell_2 & cell_2 == cell_3) {
             round_won = true;
             break; // wenn alle Zellen 1 bis 3 gleich sind also z.B. X dann ist die Runde gewonnen
         }
     }
-    if(round_won) {
+    if (round_won) {
         status_text.textContent = `${current_player} hat gewonnen!`;
         running = false;
-    }
-    else if(!legal_moves.includes("")) {
+    } else if (!legal_moves.includes("")) {
         status_text.textContent = `Unentschieden!`;
         running = false;
-    }
-    else {
+    } else {
         change_player();
     }
 }

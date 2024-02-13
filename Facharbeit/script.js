15dac94d0d5057c5f26a5c437fb86bccfab53abc
amonhaupt <amon.haupt@aol.com>
2024-02-13 18:22:01 +0100

    Update

--- Facharbeit/script.js
+++ Facharbeit/script.js
@@ -27,7 +27,6 @@ var object_model = undefined;
 
 function load_cocoSsd_model() {
     document.getElementById("still_loading").classList.remove("removed");
-    console.log("test")
     cocoSsd.load().then(function (loadedModel) {
         object_model = loadedModel;
         cam_section.classList.remove("invisible");
@@ -65,11 +64,15 @@ function enable_cam(event) {
 
     event.target.classList.add("removed");
 
+    video_box = window.innerWidth - 12;
+    
     const constraints = {
         video: {
             facingMode: {
                 exact: "environment"
-            }
+            },
+            width: video_box,
+            height: video_box
         }
     };
 
@@ -77,6 +80,24 @@ function enable_cam(event) {
         video.srcObject = stream;
         video.addEventListener("loadeddata", predict_webcam);
     });
+    document.getElementById("disable_cam_button").classList.remove("removed");
+}
+
+function disable_cam() {
+    const stream = video.srcObject;
+    const tracks = stream.getTracks();
+
+    tracks.forEach((track) => {
+        track.stop();
+    });
+
+    video.srcObject = null;
+    document.getElementById("disable_cam_button").classList.add("removed");
+    // let live_view_children = liveView.children;
+    // console.log(live_view_children);
+    liveView.remove(3);
+    liveView.remove(4);
+    
 }
 
 var english_words = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'];
@@ -314,6 +335,10 @@ const win_conditons = [
 ];
 
 let legal_moves = ["", "", "", "", "", "", "", "", ""];
+var current_legal_moves = legal_moves;
+let human_player = "X";
+let ai_player = "O";
+
 let current_player = "X";
 let running = false;
 
@@ -338,7 +363,7 @@ function cell_clicked() {
     if (legal_moves[cellIndex] != "" || !running) {
         return;
     }
-    if (current_player == "O") {
+    if (current_player == ai_player) {
         return;
     }
     update_cell(this, cellIndex);
@@ -347,24 +372,27 @@ function cell_clicked() {
 
 function update_cell(cell, index) {
     legal_moves[index] = current_player;
+    current_legal_moves = legal_moves;
     cell.textContent = current_player;
 
 }
 
 function change_player() {
-    current_player = (current_player == "X") ? "O" : "X";
-    if (current_player == "O") {
-        if (difficulty == "easy") {
-            console.log("easy");
-            setTimeout(make_easy_ai_move, 500);
-        } if (difficulty == "medium") {
-            console.log("mittel");
-            setTimeout(make_medium_ai_move, 500);
-        } if (difficulty == "hard") {
-            console.log("schwer");
-            setTimeout(make_hard_ai_move, 500);
+    console.log(current_player)
+    if (current_player == human_player) {
+        current_player = ai_player
+        if (current_player == "O") {
+            if (difficulty == "easy") {
+                setTimeout(make_easy_ai_move, 500);
+            } if (difficulty == "medium") {
+                setTimeout(make_medium_ai_move, 500);
+            } if (difficulty == "hard") {
+                make_hard_ai_move();
+            }
+            
         }
-        
+    } else {
+        current_player = human_player
     }
     status_text.textContent = `${current_player} ist dran`;
 }
@@ -373,7 +401,7 @@ function make_easy_ai_move() {
     let move = Math.floor(Math.random() * 9);
     let ai_cell = document.getElementById("cell" + move);
     if (legal_moves[move] != "") {
-        make_ai_move();
+        make_easy_ai_move();
     } else {
         legal_moves[move] = current_player;
         ai_cell.textContent = current_player;
@@ -385,7 +413,7 @@ function make_medium_ai_move() {
     let move = Math.floor(Math.random() * 9);
     let ai_cell = document.getElementById("cell" + move);
     if (legal_moves[move] != "") {
-        make_ai_move();
+        make_medium_ai_move();
     } else {
         legal_moves[move] = current_player;
         ai_cell.textContent = current_player;
@@ -393,17 +421,100 @@ function make_medium_ai_move() {
     }
 }
 
-function make_hard_ai_move() {
-    let move = Math.floor(Math.random() * 9);
-    let ai_cell = document.getElementById("cell" + move);
-    if (legal_moves[move] != "") {
-        make_ai_move();
+let move_scores = {
+    X: -1,
+    O: 1,
+    tie: 0
+};
+
+function evaluate(current_legal_moves) {
+    let round_won = false;
+    for (let i = 0; i < win_conditons.length; i++) {
+        const condition = win_conditons[i]; // i = 0 -> [0, 1, 2] obere Reihe
+        const cell_1 = current_legal_moves[condition[0]]; // cell_1 == 0
+        const cell_2 = current_legal_moves[condition[1]]; // cell_2 == 1
+        const cell_3 = current_legal_moves[condition[2]]; // cell_3 == 2
+
+        if (cell_1 == "" || cell_2 == "" || cell_3 == "") { // (|| <- steht für oder)
+            continue; // wenn eine Zelle oder mehrere leer sind neustarten mit i + 1 
+        }
+        if (cell_1 == cell_2 & cell_2 == cell_3) {
+            round_won = true;
+            break; // wenn alle Zellen 1 bis 3 gleich sind also z.B. X dann ist die Runde gewonnen
+        }
+    }
+    if (round_won) {
+        return current_player;
+    } else if (!current_legal_moves.includes("")) {
+        return "tie";
     } else {
+        return null;
+    }
+}
+
+function minimax(current_legal_moves, depth, is_maximizing) {
+    let result = evaluate(current_legal_moves);
+    if (result !== null) {
+        let score = move_scores[result]
+        return score;
+    }
+    if (is_maximizing) {
+        let best_score = -Infinity;
+        for (let i = 0; i < 9; i++) {
+            if (current_legal_moves[i] == "") {
+                current_legal_moves[i] = ai_player;
+                let given_score = minimax(current_legal_moves, depth + 1, false);
+                current_legal_moves[i] = "";
+                best_score = Math.max(given_score, best_score);
+            }
+        }
+        return best_score;
+    } else {
+        let best_score = Infinity;
+        for (let i = 0; i < 9; i++) {
+            if (current_legal_moves[i] == "") {
+                current_legal_moves[i] = human_player;
+                let given_score = minimax(current_legal_moves, depth + 1, true);
+                current_legal_moves[i] = "";
+                best_score = Math.min(given_score, best_score);
+            }
+        }
+        return best_score;
+    }
+}
+
+function best_move() {
+    // console.log("best_move")
+    let best_score = -Infinity;
+    let move_to_make;
+    for (let i = 0; i < 9; i++) {
+        if (current_legal_moves[i] == "") {
+            current_legal_moves[i] = ai_player;
+            let given_score = minimax(current_legal_moves, 0, false);
+            current_legal_moves[i] = "";
+            if (given_score > best_score) {
+                best_score = given_score;
+                move_to_make = i;
+            }
+        }
+    }
+    return move_to_make; 
+}
+
+function make_hard_ai_move() {
+    if (current_player == human_player) {
+        return;
+    } else {
+        let move = best_move();
+        let ai_cell = document.getElementById("cell" + move);
         legal_moves[move] = current_player;
+        current_legal_moves = legal_moves;
         ai_cell.textContent = current_player;
         check_for_winner();
     }
+
 }
+
 function check_for_winner() {
     let round_won = false;
     for (let i = 0; i < win_conditons.length; i++) {
@@ -423,11 +534,14 @@ function check_for_winner() {
     if (round_won) {
         status_text.textContent = `${current_player} hat gewonnen!`;
         running = false;
+        return current_player;
     } else if (!legal_moves.includes("")) {
         status_text.textContent = `Unentschieden!`;
         running = false;
+        return "tie";
     } else {
         change_player();
+        return null;
     }
 }
 
